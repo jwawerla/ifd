757610c81d12962997bb4f1693c91b8ea66dcc8a
jwawerla <jwawerla@r2d2.(none)>
2009-11-10 07:58:36 -0800

    adopted to the latest stage

---
+++ src/argparser.cpp
@@ -0,0 +1,23 @@
+//
+// C++ Implementation: argparser
+//
+// Description: 
+//
+//
+// Author:  <>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "argparser.h"
+
+CArgParser::CArgParser()
+{
+}
+
+
+CArgParser::~CArgParser()
+{
+}
+
+
---
+++ src/argparser.h
@@ -0,0 +1,28 @@
+//
+// C++ Interface: argparser
+//
+// Description: 
+//
+//
+// Author:  <>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef ARGPARSER_H
+#define ARGPARSER_H
+
+/**
+Simple parser for command line parameter strings comming from Stage
+
+	@author 
+*/
+class CArgParser{
+public:
+    CArgParser();
+
+    ~CArgParser();
+
+};
+
+#endif
